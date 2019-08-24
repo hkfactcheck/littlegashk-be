4bdb7af28d98b02a5a2dbcf43247956060cca176
hkfactcheck <hkfactcheck@gmail.com>
2019-08-24 23:13:03 +0800

    topics/url endpoint to return topic objects

--- src/main/java/io/littlegashk/webapp/TopicController.java
+++ src/main/java/io/littlegashk/webapp/TopicController.java
@@ -1,6 +1,7 @@
 package io.littlegashk.webapp;
 
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 import io.littlegashk.webapp.entity.*;
 import io.littlegashk.webapp.repository.ChildRelationRepository;
 import io.littlegashk.webapp.repository.TagRepository;
@@ -13,13 +14,13 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 @RestController
 @RequestMapping("/topics")
@@ -43,7 +44,7 @@ public class TopicController {
     @ApiOperation("get all topics, sorted by lastUpdated date desc")
     @GetMapping
     public ResponseEntity<Page<Topic>> getTopics(@ApiParam(example = "1565883488250") @RequestParam(required = false) Long lastUpdated,
-                                                 @ApiParam(example= "0") @RequestParam(required = false, defaultValue = "0") Integer page) {
+                                                 @ApiParam(example = "0") @RequestParam(required = false, defaultValue = "0") Integer page) {
 
         Page<Topic> allTopic = repository.getAllTopicUpdatedBefore(lastUpdated == null ? Long.MAX_VALUE : lastUpdated, page);
         return ResponseEntity.ok(allTopic);
@@ -60,10 +61,16 @@ public class TopicController {
 
     @ApiOperation("get all topics with specified date, sorted by topicId desc")
     @GetMapping("/url")
-    public ResponseEntity<List<String>> getTopicIdsByUrl(@ApiParam(example = "https://google.com") @RequestParam String url) {
+    public ResponseEntity<List<Topic>> getTopicsByUrl(@ApiParam(example = "https://google.com") @RequestParam String url) {
 
         Page<UrlTopic> allTopic = urlRepository.findAllWithUrl(url.trim(), 0);
-        return ResponseEntity.ok(allTopic.getContent().stream().map(UrlTopic::getTopicId).collect(Collectors.toList()));
+        Iterable<Topic> topics = repository.findAllById(allTopic.get()
+                                       .map(UrlTopic::getTopicId)
+                                       .flatMap(s -> Stream.of(TopicId.of(s, EntryType.TOPIC),
+                                                               TopicId.of(s, EntryType.PROGRESS),
+                                                               TopicId.of(s, EntryType.RESPONSE)))
+                                       .collect(Collectors.toList()));
+        return ResponseEntity.ok(Lists.newArrayList(topics));
     }
 
     @ApiOperation("get specific topic with topicId")
@@ -77,13 +84,13 @@ public class TopicController {
     @GetMapping("/{topicId}/progress")
     public ResponseEntity<Page<Topic>> getAllTopicProgress(@ApiParam(example = "2019-08-01|1565877016020") @PathVariable String topicId,
                                                            @RequestParam(required = false, defaultValue = "9999") String lastChildId,
-                                                           @ApiParam(example= "0") @RequestParam(required = false, defaultValue = "0") Integer page) {
+                                                           @ApiParam(example = "0") @RequestParam(required = false, defaultValue = "0") Integer page) {
 
         Page<ChildRelation> relations = childRelationRepository.getProgress(topicId, lastChildId, page);
         List<TopicId> childIds = relations.stream()
-                                         .map(ChildRelation::getChildTopicId)
-                                         .map(s->TopicId.of(s, EntryType.PROGRESS))
-                                         .collect(Collectors.toList());
+                                          .map(ChildRelation::getChildTopicId)
+                                          .map(s -> TopicId.of(s, EntryType.PROGRESS))
+                                          .collect(Collectors.toList());
         Iterable<Topic> topics = repository.findAllById(childIds);
         return ResponseEntity.ok(new PageImpl<>(ImmutableList.copyOf(topics), relations.getPageable(), relations.getTotalElements()));
     }
@@ -92,11 +99,12 @@ public class TopicController {
     @GetMapping("/{topicId}/response")
     public ResponseEntity<Page<Topic>> getAllTopicResponse(@ApiParam(example = "2019-08-01|1565877016020") @PathVariable String topicId,
                                                            @RequestParam(required = false, defaultValue = "9999") String lastChildId,
-                                                           @ApiParam(example= "0") @RequestParam(required = false, defaultValue = "0") Integer page) {
+                                                           @ApiParam(example = "0") @RequestParam(required = false, defaultValue = "0") Integer page) {
+
         Page<ChildRelation> relations = childRelationRepository.getResponse(topicId, lastChildId, page);
         List<TopicId> childIds = relations.stream()
                                           .map(ChildRelation::getChildTopicId)
-                                          .map(s->TopicId.of(s, EntryType.RESPONSE))
+                                          .map(s -> TopicId.of(s, EntryType.RESPONSE))
                                           .collect(Collectors.toList());
         Iterable<Topic> topics = repository.findAllById(childIds);
         return ResponseEntity.ok(new PageImpl<>(ImmutableList.copyOf(topics), relations.getPageable(), relations.getTotalElements()));
